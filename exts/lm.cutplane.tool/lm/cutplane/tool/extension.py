879ba154c1d77c85623728441e988aaa99e5f386
Brett-Amberge <brett.j.amberge@lmco.com>
2022-08-18 10:42:35 -0400

    Basic UI setup

--- exts/lm.cutplane.tool/lm/cutplane/tool/extension.py
+++ exts/lm.cutplane.tool/lm/cutplane/tool/extension.py
@@ -1,6 +1,7 @@
 import omni.ext
 import omni.ui as ui
-
+import omni.kit.commands
+from pxr import Sdf, Gf
 
 # Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
 def some_public_function(x: int):
@@ -17,27 +18,29 @@ class MyExtension(omni.ext.IExt):
     def on_startup(self, ext_id):
         print("[omni.hello.world] MyExtension startup")
 
-        self._count = 0
+        self._window = ui.Window("Cut plane tool", width=300, height=300)
 
-        self._window = ui.Window("My Window", width=300, height=300)
+        def spawnPlane(isOn, axis):
+            if isOn:
+                omni.kit.commands.execute('CreateMeshPrimWithDefaultXform', prim_type='Plane')
+                omni.kit.commands.execute('MovePrims',
+	                paths_to_move={'/World/Plane': '/World/' + axis + 'Plane'})
+            else:
+                omni.kit.commands.execute('DeletePrims', paths=['/World/' + axis + 'Plane'])
+                
+        def plane_stack(lab, axis):
+            with ui.HStack():
+                label = ui.Label(lab)
+                box = ui.CheckBox()
+                box.model.add_value_changed_fn(lambda x: spawnPlane(box.model.get_value_as_bool(), axis))
+                field = ui.MultiFloatDragField(0.0)
+                field.model.add_item_changed_fn(lambda x,y: movePlane(box.model.get_value_as_bool(), axis))
+
         with self._window.frame:
             with ui.VStack():
-                label = ui.Label("")
-                
-
-                def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
-
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
-
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+                x = plane_stack("X-axis:", "X")
+                y = plane_stack("Y-axis:", "Y")
+                z = plane_stack("Z-axis:", "Z")
 
     def on_shutdown(self):
         print("[omni.hello.world] MyExtension shutdown")
