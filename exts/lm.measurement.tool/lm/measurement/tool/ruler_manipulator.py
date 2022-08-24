c8f670dc11b960c31465dd6edecd673ab6ac7ada
Brett-Amberge <brett.j.amberge@lmco.com>
2022-08-24 15:52:21 -0400

    Added ability to draw multiple lines, clear them with double click

--- exts/lm.measurement.tool/lm/measurement/tool/ruler_manipulator.py
+++ exts/lm.measurement.tool/lm/measurement/tool/ruler_manipulator.py
@@ -17,34 +17,46 @@ class ToolType(Enum):
     RULER = 1
     ANGLE = 2
 
-class _ClickGesture(sc.ClickGesture):
-    def __init__(self, manipulator: sc.Manipulator, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._start = False
-        self.__manipulator = manipulator
-        self._manager = Manager()
-
-    def on_ended(self):
-        # Update the line whenever a click happens
-        if self.__manipulator._tool.value > 0:
-            self._start = not self._start
-            model = self.__manipulator.model
-            point = self.sender.gesture_payload.ray_closest_point
-
-            if self._start:
-                model.set_floats(model.startpoint, point)
-            else:
-                model.set_floats(model.endpoint, point)
-                model.calculate_dist()
-                self.__manipulator.invalidate() # Redraw the line
-
 class Manager(sc.GestureManager):
     def __init__(self):
         super().__init__()
 
     def should_prevent(self, gesture, preventer):
-        if preventer.__manipulator._tool.value > 0:
+        if preventer._name == "Double":
             return True
+
+mgr = Manager()
+
+class _ClickGesture(sc.ClickGesture):
+    def __init__(self, manipulator: sc.Manipulator, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._name = "Left"
+        self._start = False
+        self.__manipulator = manipulator
+        self._manager = mgr
+
+    def on_ended(self):
+        # Update the line whenever a click happens
+        if self.__manipulator._tool.value > 0: # Check if the ruler tool is enabled
+            self._start = not self._start
+            model = self.__manipulator.model
+            point = self.sender.gesture_payload.ray_closest_point
+
+            model.add_point(point)
+            model.calculate_dist(model.points[0], model.points[-1])
+            self.__manipulator.invalidate() # Redraw the line
+
+class _DoubleClickGesture(sc.DoubleClickGesture):
+    def __init__(self, manipulator, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._name = "Double"
+        self.__manipulator = manipulator
+        self._manager = mgr
+
+    def on_ended(self):
+        model = self.__manipulator.model
+        model.clear_points()
+        self.__manipulator.invalidate()
 
 class RulerManipulator(sc.Manipulator):
 
@@ -58,13 +70,13 @@ class RulerManipulator(sc.Manipulator):
             self.model = RulerModel()
 
         # Set the gesture on the screen
-        sc.Screen(gestures=self.gestures or [_ClickGesture(weakref.proxy(self))])
+        sc.Screen(gestures=self.gestures or [_ClickGesture(weakref.proxy(self)), _DoubleClickGesture(weakref.proxy(self))])
 
         self._draw_shape()
 
         # Position the distance label above the center of the line
         if self.model.dist.value > 0.0:
-            position = self.model.get_midpoint()
+            position = self.model.get_midpoint(self.model.points[0], self.model.points[1])
             with sc.Transform(transform=sc.Matrix44.get_translation_matrix(*position)):
                 with sc.Transform(look_at=sc.Transform.LookAt.CAMERA):
                     with sc.Transform(scale_to=sc.Space.SCREEN):
@@ -82,7 +94,11 @@ class RulerManipulator(sc.Manipulator):
         if not self.model:
             return
         if self.model.startpoint and self.model.endpoint:
-            sc.Line(self.model.startpoint.value, self.model.endpoint.value)
+            i = 0
+            points = self.model.points
+            while i < (len(points) - 1):
+                sc.Line(points[i], points[i+1])
+                i += 1
 
     def set_tool(self):
         if self._tool.value == 0:
