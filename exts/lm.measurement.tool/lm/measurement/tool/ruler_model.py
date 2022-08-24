891a07f22f7bad236a6f6f73536dc7acc776530f
Brett-Amberge <brett.j.amberge@lmco.com>
2022-08-24 10:32:41 -0400

    Added functions to calculate distance and midpoint

--- exts/lm.measurement.tool/lm/measurement/tool/ruler_model.py
+++ exts/lm.measurement.tool/lm/measurement/tool/ruler_model.py
@@ -6,6 +6,7 @@ import carb
 
 class RulerModel(sc.AbstractManipulatorModel):
 
+    # Subclasses for storing model information
     class PositionItem(sc.AbstractManipulatorItem):
         def __init__(self):
             super().__init__()
@@ -23,13 +24,16 @@ class RulerModel(sc.AbstractManipulatorModel):
         self.dist = RulerModel.FloatItem()
 
     def get_as_floats(self, item):
+        # Return the value of the given item
         return item.value
 
     def set_floats(self, item, value):
+        # Change the value of the given item
         item.value = value
-        self._item_changed(item)
+        self._item_changed(item) # Tells the manipulator the model changed
 
     def calculate_dist(self):
+        # Find the distance between the two points
         x1,y1,z1 = self.startpoint.value
         x2,y2,z2 = self.endpoint.value
 
@@ -37,6 +41,14 @@ class RulerModel(sc.AbstractManipulatorModel):
 
         self.set_floats(self.dist, distance)
     
+    def get_midpoint(self):
+        # Find the middle of the line
+        x1,y1,z1 = self.startpoint.value
+        x2,y2,z2 = self.endpoint.value
+
+        midpoint = [(x2+x1)/2, (y2+y1)/2, (z2+z1)/2]
+        #print(str(self.startpoint.value) +  " " + str(midpoint) + " " + str(self.endpoint.value))
+        return midpoint
 
         
 
